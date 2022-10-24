fa7b1b35b6ea141a4309ee78f7db772f8f02ab79
Jubiko31 <jubiko.macaberidze1@gmail.com>
2022-10-24 17:50:07 +0400

    article information and details

--- components/index.js
+++ components/index.js
@@ -3,3 +3,7 @@ export { default as Categories } from './Categories/Categories';
 export { default as PostWidget } from './Widgets/PostWidget';
 export { default as Header } from './Header';
 export { default as Layout } from './Layout';
+export { default as PostDetail } from './Article/PostDetail';
+export { default as Author } from './Article/Author';
+export { default as CommentsForm } from './Article/Comments/CommentsForm';
+export { default as Comment } from './Article/Comments/Comment'; 
--- services/index.js
+++ services/index.js
@@ -36,6 +36,39 @@ export const getPosts = async () => {
     const result = await request(graphqlAPI, query)
     return result.postsConnection.edges
 }
+export const getPostDetails = async (slug) => {
+    const query = gql`
+        query GetPostDetails($slug: String!) {
+          post(where: { slug: $slug }) {
+            author {
+              bio
+              id
+              name
+              photo {
+                url
+              }
+            }
+            createdAt
+            slug
+            title
+            excerpt
+            featuredImage {
+              url
+            }
+            categories {
+              name
+              slug
+            }
+            content {
+              raw
+            }
+          }
+        }
+    `
+
+    const result = await request(graphqlAPI, query, { slug } );
+    return result.post;
+}
 
 export const getRecentPosts = async () => {
   const query = gql`
@@ -58,17 +91,24 @@ export const getRecentPosts = async () => {
   return result.posts;
 }
 
-export const getSimilarPosts = async () => {
+export const getSimilarPosts = async (categories, slug) => {
   const query = gql`
     query getPostDetails($slug: String!, $categories: [String!]) {
       posts(
-        where: { slug_not: $slug, AND { categories_some: { slug_in: $categories } } }
+        where: { slug_not: $slug, AND: { categories_some: { slug_in: $categories } } }
         last: 3
-      )
+      ) {
+        title
+        featuredImage {
+          url
+        }
+        createdAt
+        slug
+      }
     }
   `
 
-  const result = await request(graphqlAPI, query)
+  const result = await request(graphqlAPI, query, { categories, slug })
   return result.posts
 }
 
